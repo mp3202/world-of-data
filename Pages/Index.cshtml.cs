54b21ed4d30599c89d1ae97a924819133e1a0f2e
Mia Popovic <mpopovic1@buffs.wtamu.edu>
2023-05-10 18:52:21 -0500

    push2

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using world_of_data.Models;
 
 namespace world_of_data.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly WoWClassDbContext _context; // Replaces the "db" variable from before
     private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public List<WoWClass> WoWClasses {get; set;} = default!;
+    public IndexModel(WoWClassDbContext context, ILogger<IndexModel> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
     public void OnGet()
     {
-
+        WoWClasses = _context.WoWClasses.ToList();
     }
 }
