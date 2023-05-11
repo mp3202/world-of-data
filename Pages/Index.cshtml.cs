5c8b9cc0b063c241b103d9996ad312836edad757
Mia Popovic <mpopovic1@buffs.wtamu.edu>
2023-05-11 18:12:20 -0500

    FINAL PROJECT FINAL!!

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using world_of_data.Models;
 
 namespace world_of_data.Pages
@@ -14,21 +15,30 @@ namespace world_of_data.Pages
     {
         private readonly WoWClassDbContext _context;
         private readonly ILogger<IndexModel> _logger;
-        public List<WoWClass> WoWClasses {get; set;} = default!;
         public List<Character> Characters {get; set;} = default!;
+        public IList<WoWClass> WoWClasses { get;set; } = default!;
 
-        public WoWClass WoWClass {get; set;}
-        public Character Character {get; set;} = default!;
-
+        [BindProperty(SupportsGet = true)]
+        public int PageNum {get; set;} = 1;
+        public int PageSize {get; set;} = 9;
         public IndexModel(WoWClassDbContext context, ILogger<IndexModel> logger)
         {
             _context = context;
             _logger = logger;
         }
 
-        public void OnGet()
+
+        public async Task OnGetAsync()
         {
-            WoWClasses = _context.WoWClass.ToList();
+            if (_context.WoWClass != null)
+            {
+                WoWClasses = await _context.WoWClass.ToListAsync();
+
+                var query = _context.WoWClass.Select(p => p);
+
+                WoWClasses = await query.Skip((PageNum-1)*PageSize).Take(PageSize).ToListAsync();
+            }
+
             Characters = _context.Character.ToList();
         }
     }
--- Pages/Players/Index.cshtml.cs
+++ Pages/Players/Index.cshtml.cs
@@ -21,10 +21,6 @@ namespace world_of_data.Pages.Players
             // _logger = logger;
         }
 
-        [BindProperty(SupportsGet = true)]
-        public int PageNum {get; set;} = 1;
-        public int PageSize {get; set;} = 10;
-
         // Sorting support
         [BindProperty(SupportsGet = true)]
         public string CurrentSort {get; set;} = string.Empty;
@@ -35,61 +31,136 @@ namespace world_of_data.Pages.Players
         {
             if (_context.WoWClass != null)
             {
-                // WoWClass = await _context.WoWClass.ToListAsync();
-                WoWClass = await _context.WoWClass.Include(w => w.Characters).ToListAsync();
+                WoWClass = await _context.WoWClass.ToListAsync();
                 Character = await _context.Character.ToListAsync();
-                // Character = _context.Character.ToList();
              
-                // Sorting support
-                // var query = _context.WoWClass.Include(p => p.Characters).Select(p => p);
-                var query = _context.WoWClass.Select(p => p);
+                //FILTER BY: (Armor Type: PLATE, MAIL, LEATHER, CLOTH), (Role: DPS, HEALER, TANK)
+                var query = _context.WoWClass.Include(c=>c.Characters).Select(p => p);
+                // var query_char = _context.WoWClass.SelectMany(wowClass => wowClass.Characters);
                 List<SelectListItem> sortItems = new List<SelectListItem> {
-                    new SelectListItem { Text = "iLVL Ascending", Value = "ilvl_asc" },
-                    new SelectListItem { Text = "iLVL Descending", Value = "ilvl_desc"},
+                    // by armor type
+                    new SelectListItem { Text = "Armor: PLATE", Value = "plate_sort" },
+                    new SelectListItem { Text = "Armor: MAIL", Value = "mail_sort"},
+                    new SelectListItem { Text = "Armor: LEATHER", Value = "leather_sort" },
+                    new SelectListItem { Text = "Armor: CLOTH", Value = "cloth_sort"},
 
-                    new SelectListItem { Text = "2v2 Ascending", Value = "arena2v2_asc" },
-                    new SelectListItem { Text = "2v2 Descending", Value = "arena2v2_desc"},
+                    // by role
+                    new SelectListItem { Text = "Role: HEALER", Value = "healer_sort" },
+                    new SelectListItem { Text = "Role: DPS", Value = "dps_sort"},
+                    new SelectListItem { Text = "Role: TANK", Value = "tank_sort"},
 
-                    new SelectListItem { Text = "3v3 Ascending", Value = "arena3v3_asc" },
-                    new SelectListItem { Text = "3v3 Descending", Value = "arena3v3_desc"},
+                    // // by name/realm/class/spec a-z
+                    // new SelectListItem { Text = "Realm A-Z", Value = "realm_asc" },
+                    // new SelectListItem { Text = "Realm Z-A", Value = "realm_desc"},
+
+                    // new SelectListItem { Text = "Name A-Z", Value = "name_asc" },
+                    // new SelectListItem { Text = "Name Z-A", Value = "name_desc"},
+
+                    // new SelectListItem { Text = "Class A-Z", Value = "class_asc" },
+                    // new SelectListItem { Text = "Class Z-A", Value = "class_desc"},
+
+                    // new SelectListItem { Text = "Spec A-Z", Value = "spec_asc" },
+                    // new SelectListItem { Text = "Spec Z-A", Value = "spec_desc"},
+
+                    // // by ilvl,  arena/mythic scores
+                    // new SelectListItem { Text = "2v2 Ascending", Value = "arena2v2_asc" },
+                    // new SelectListItem { Text = "2v2 Descending", Value = "arena2v2_desc"},
+
+                    // new SelectListItem { Text = "3v3 Ascending", Value = "arena3v3_asc" },
+                    // new SelectListItem { Text = "3v3 Descending", Value = "arena3v3_desc"},
                     
-                    new SelectListItem { Text = "Mythic Ascending", Value = "mythic_asc" },
-                    new SelectListItem { Text = "Mythic Descending", Value = "mythic_desc"}
+                    // new SelectListItem { Text = "Mythic Ascending", Value = "mythic_asc" },
+                    // new SelectListItem { Text = "Mythic Descending", Value = "mythic_desc"}
                 };
                 SortList = new SelectList(sortItems, "Value", "Text", CurrentSort);
 
                 switch (CurrentSort)
-                { // asc/desc for ilvl, arena scores, mythic scores
-                    case "ilvl_asc":
-                        query = query.OrderBy(p => p.Characters.Min(c => c.iLVL));
+                {
+                    // ALPHABETICAL SORTING
+                    // case "name_asc":
+                    //     query_char = query_char.OrderBy(p => p.charName);
+                    //     break;
+                    // case "name_desc":
+                    //     query_char = query_char.OrderByDescending(p => p.charName);
+                    //     break;
+
+                    // case "realm_asc":
+                    //     query_char = query_char.OrderBy(p => p.Realm);
+                    //     break;
+                    // case "realm_desc":
+                    //     query_char = query_char.OrderByDescending(p => p.Realm);
+                    //     break;
+
+                    // case "class_asc":
+                    //     query = query.OrderBy(p => p.className);
+                    //     break;
+                    // case "class_desc":
+                    //     query = query.OrderByDescending(p => p.className);
+                    //     break;
+
+                    // case "spec_asc":
+                    //     query = query.OrderBy(p => p.classSpec);
+                    //     break;
+                    // case "spec_desc":
+                    //     query = query.OrderByDescending(p => p.classSpec);
+                    //     break;
+
+                    // // FOR SCORES SORTING
+                    // case "ilvl_asc":
+                    //     query_char = query_char.OrderBy(p => p.iLVL);
+                    //     break;
+                    // case "ilvl_desc":
+                    //     query_char = query_char.OrderByDescending(p => p.iLVL);
+                    //     break;
+
+                    // case "arena2v2_asc":
+                    //     query_char = query_char.OrderBy(p => p.Arena2v2);
+                    //     break;
+                    // case "arena2v2_desc":
+                    //     query_char = query_char.OrderByDescending(p => p.Arena2v2);
+                    //     break;
+
+                    // case "arena3v3_asc":
+                    //     query_char = query_char.OrderBy(p => p.Arena3v3);
+                    //     break;
+                    // case "arena3v3_desc":
+                    //     query_char = query_char.OrderByDescending(p => p.Arena3v3);
+                    //     break;
+
+                    // case "mythic_asc":
+                    //     query_char = query_char.OrderBy(p => p.MythicScore);
+                    //     break;
+                    // case "mythic_desc":
+                    //     query_char = query_char.OrderByDescending(p => p.MythicScore);
+                    //     break;
+
+                    // for filtering
+                    case "plate_sort":
+                        query = query.Select(s=>s).Where(c => c.classArmor.Contains("Plate"));
                         break;
-                    case "ilvl_desc":
-                        WoWClass = WoWClass.OrderByDescending(w => w.Characters.Min(c => c.iLVL)).ToList();
-                        // query = query.OrderByDescending(p => p.Characters.Min(c => c.iLVL));
-                        // query = query.OrderByDescending(p => p.Characters.Select(c => c.iLVL).Min());
+                    case "mail_sort":
+                        query = query.Where(c => c.classArmor.Contains("Mail"));  
+                        break;
+                    case "leather_sort":
+                        query = query.Where(c => c.classArmor.Contains("Leather"));
+                        break;
+                    case "cloth_sort":
+                        query = query.Where(c => c.classArmor.Contains("Cloth"));
                         break;
 
-                    case "arena2v2_asc":
-                        query = query.OrderBy(p => p.Characters.Min(c => c.Arena2v2));
+                    case "healer_sort":
+                        query = query.Where(c => c.classRole.Contains("Healer"));
                         break;
-                    case "arena2v2_desc":
-                        query = query.OrderByDescending(p => p.Characters.Min(c => c.Arena2v2));
+                    case "dps_sort":
+                        query = query.Where(c => c.classRole.Contains("DPS"));
                         break;
-                    case "arena3v3_asc":
-                        query = query.OrderBy(p => p.Characters.Min(c => c.Arena3v3));
-                        break;
-                    case "arena3v3_desc":
-                        query = query.OrderByDescending(p => p.Characters.Min(c => c.Arena3v3));
-                        break;
-                    case "mythic_asc":
-                        query = query.OrderBy(p => p.Characters.Min(c => c.MythicScore));
-                        break;
-                    case "mythic_desc":
-                        query = query.OrderByDescending(p => p.Characters.Min(c => c.MythicScore));
+                    case "tank_sort":
+                        query = query.Where(c => c.classRole.Contains("Tank"));
                         break;
                 }
 
-                WoWClass = await query.Skip((PageNum-1)*PageSize).Take(PageSize).ToListAsync();
+                WoWClass = await query.ToListAsync();
+                
             }
 
         }
