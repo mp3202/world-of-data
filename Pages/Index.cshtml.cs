664f38f8d8996cd607196cad7843b1b587c12612
Mia Popovic <mpopovic1@buffs.wtamu.edu>
2023-05-10 21:56:12 -0500

    umm

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,25 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using world_of_data.Models;
 
-namespace world_of_data.Pages;
-
-public class IndexModel : PageModel
+namespace world_of_data.Pages
 {
-    private readonly WoWClassDbContext _context; // Replaces the "db" variable from before
-    private readonly ILogger<IndexModel> _logger;
-    public List<WoWClass> WoWClasses {get; set;} = default!;
-    public List<Character> Characters {get; set;} = default!;
-
-    public IndexModel(WoWClassDbContext context, ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _context = context;
-        _logger = logger;
-    }
+        private readonly world_of_data.Models.WoWClassDbContext _context;
 
-    public void OnGet()
-    {
-        WoWClasses = _context.WoWClass.ToList();
+        public IndexModel(world_of_data.Models.WoWClassDbContext context)
+        {
+            _context = context;
+        }
+
+        public WoWClass WoWClass { get; set; }
+        
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Round 3: Add .Include() to bring in courses
+            WoWClass = await _context.WoWClass.Include(p => p.Characters).FirstOrDefaultAsync(m => m.WoWClassID == id);
+            
+            if (WoWClass == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
     }
 }
