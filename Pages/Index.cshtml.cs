c37116ec1de4d4111c70896cbde94ac2abb721d6
Mia Popovic <mpopovic1@buffs.wtamu.edu>
2023-05-10 23:06:37 -0500

    lol

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using world_of_data.Models;
 
@@ -11,31 +12,39 @@ namespace world_of_data.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly world_of_data.Models.WoWClassDbContext _context;
+        private readonly WoWClassDbContext _context;
+        private readonly ILogger<IndexModel> _logger;
+        public List<WoWClass> WoWClasses {get; set;} = default!;
+        public List<Character> Characters {get; set;} = default!;
 
-        public IndexModel(world_of_data.Models.WoWClassDbContext context)
+        public WoWClass WoWClass {get; set;}
+        public Character Character {get; set;} = default!;
+
+        public IndexModel(WoWClassDbContext context, ILogger<IndexModel> logger)
         {
             _context = context;
+            _logger = logger;
         }
+        // public async Task<IActionResult> OnGetAsync(int? id)
+        // {
+        //     if (id == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-        public WoWClass WoWClass { get; set; }
-        
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            // Round 3: Add .Include() to bring in courses
-            WoWClass = await _context.WoWClass.Include(p => p.Characters).FirstOrDefaultAsync(m => m.WoWClassID == id);
+        //     // Round 3: Add .Include() to bring in courses
+        //     WoWClass = await _context.WoWClass.Include(c => c.Characters).FirstOrDefaultAsync(m => m.WoWClassID == id);
             
-            if (WoWClass == null)
-            {
-                return NotFound();
-            }
-            return Page();
+        //     if (WoWClass == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     return Page();
+        // }
+        public void OnGet()
+        {
+            WoWClasses = _context.WoWClass.ToList();
+            Characters = _context.Character.ToList();
         }
-
     }
 }
